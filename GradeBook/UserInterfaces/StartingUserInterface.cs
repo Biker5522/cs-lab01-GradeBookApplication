80d980f4e824ac7aa103bd56d2d44805511ff505
Kadath <dawidcichy34@gmail.com>
2022-03-13 18:39:16 +0100

    5 Update StartingUserInterface's CreateCommand method

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,15 +34,34 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
+
             var name = parts[1];
             BaseGradeBook gradeBook = new BaseGradeBook(name);
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
+
+            if (name == "standard")
+            {
+                BaseGradeBook sgradeBook = new StandardGradeBook(name);
+                Console.WriteLine("Created standardgradebook {0}.", name);
+                GradeBookUserInterface.CommandLoop(sgradeBook);
+            }
+            else if (name == "ranked")
+            {
+                BaseGradeBook rgradeBook = new StandardGradeBook(name);
+                Console.WriteLine("Created standardgradebook {0}.", name);
+                GradeBookUserInterface.CommandLoop(rgradeBook);
+
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a supported type of gradebook, please try again", name);
+            }
         }
 
         public static void LoadCommand(string command)
