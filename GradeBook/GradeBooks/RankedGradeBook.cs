9bde1d2f9e7b2b3d151b127ecddb7dcc4039ece7
Kadath <dawidcichy34@gmail.com>
2022-04-01 17:59:13 +0200

    11 Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -33,5 +33,49 @@ namespace GradeBook.GradeBooks
                 base.CalculateStudentStatistics(name);
             }
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            List<double> oceny = new List<double>();
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            foreach (Student stu in Students)
+            {
+                oceny.Add(stu.AverageGrade);
+            }
+            oceny.Sort();
+            oceny.Reverse();
+            foreach (Student stu in Students)
+            {
+                int procent = Convert.ToInt32((20.0 / 100.0) * Students.Count);
+                for (int i = 0; i <= procent; i++)
+                {
+                    if (averageGrade == oceny[i])
+                    {
+                        return 'A';
+                    }
+                    else if (averageGrade == oceny[(i + procent)])
+                    {
+                        return 'B';
+                    }
+                    else if (averageGrade == oceny[(i + procent + procent)])
+                    {
+                        return 'C';
+                    }
+                    else if (averageGrade == oceny[(i + procent + procent + procent)])
+                    {
+                        return 'D';
+                    }
+                    else
+                    {
+                        return 'F';
+                    }
+                }
+            }
+
+            return 'F';
+        }
     }
 }
